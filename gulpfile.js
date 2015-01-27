9de8774843955df26d40a8b6d4ab5d66d639853e
Stan Lewis <slewis@fusesource.com>
2015-01-27 16:03:51 -0500

    Add missing icon files for the JMX tree

--- gulpfile.js
+++ gulpfile.js
@@ -129,6 +129,49 @@ gulp.task('reload', function() {
     .pipe(plugins.connect.reload());
 });
 
+gulp.task('embed-images', ['concat'], function() {
+
+  var replacements = [];
+
+  var files = glob.sync('img/**/*.{png,svg,gif,jpg}');
+  //console.log("files: ", files);
+  function escapeRegExp(str) {
+    return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+  }
+
+  function getDataURI(filename) {
+    var relative = path.relative('.', filename);
+    var ext = path.extname(filename);
+    var mime = 'image/jpg';
+    switch (ext) {
+      case '.png':
+        mime = 'image/png';
+      break;
+      case '.svg':
+        mime = 'image/svg+xml';
+      break;
+      case '.gif':
+        mime='image/gif';
+      break;
+    }
+    var buf = fs.readFileSync(filename);
+    return 'data:' + mime + ';base64,' + base64.encode(buf);
+  }
+
+  files.forEach(function(file) {
+    replacements.push({
+      match: new RegExp(escapeRegExp(file), 'g'),
+      replacement: getDataURI(file)
+    }); 
+  });
+
+  gulp.src('dist/dynatree-icons.css')
+  .pipe(plugins.replaceTask({
+    patterns: replacements
+  }))
+  .pipe(gulp.dest(config.dist));
+});
+
 gulp.task('build', ['bower', 'path-adjust', 'tsc', 'template', 'concat', 'clean']);
 
 gulp.task('default', ['connect']);
