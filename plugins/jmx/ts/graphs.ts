6c8b827dcae94b86bcf9784151cd18b9c4605491
Claus Ibsen <claus.ibsen@gmail.com>
2015-04-03 10:23:13 +0200

    Show Camel inflight counter in route diagram

--- plugins/jmx/ts/graphs.ts
+++ plugins/jmx/ts/graphs.ts
@@ -251,6 +251,14 @@ module Core {
             .attr("dy", 0)
             .text(_counterFunction);
 
+    var inflights = nodes
+            .append("text")
+            .attr("text-anchor", "middle")
+            .attr("class", "inflight")
+            .attr("x", 10)
+            .attr("dy", -32)
+            .text(_inflightFunction);
+
     // Append text
     var labels = nodes
             .append("text")
@@ -384,6 +392,7 @@ module Core {
   export function dagreUpdateGraphData(data) {
     var svg = d3.select("svg");
     svg.selectAll("text.counter").text(_counterFunction);
+    svg.selectAll("text.inflight").text(_inflightFunction);
 
     // add tooltip
     svg.selectAll("g .node title").text(function (d) {
@@ -402,6 +411,10 @@ module Core {
 
   function _counterFunction(d) {
     return d.counter || "";
+  }
+
+  function _inflightFunction(d) {
+    return d.inflight || "";
   }
 
 }
