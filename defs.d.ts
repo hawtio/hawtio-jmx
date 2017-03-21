6141755058673f52c8fb38609bbf34f584b5678d
Alexandre Kieling <abrianik@redhat.com>
2017-03-21 12:59:07 -0300

    add bottom margin to table views

--- defs.d.ts
+++ defs.d.ts
@@ -15,8 +15,6 @@
 /// <reference path="d.ts/jmx/ts/donutChart.d.ts"/>
 /// <reference path="d.ts/jmx/ts/mbeans.d.ts"/>
 /// <reference path="d.ts/jmx/ts/operations.d.ts"/>
-/// <reference path="d.ts/threads/ts/threadsPlugin.d.ts"/>
-/// <reference path="d.ts/threads/ts/threads.d.ts"/>
 /// <reference path="d.ts/jvm/ts/connect.d.ts"/>
 /// <reference path="d.ts/jvm/ts/discover.d.ts"/>
 /// <reference path="d.ts/jvm/ts/header.d.ts"/>
@@ -24,3 +22,5 @@
 /// <reference path="d.ts/jvm/ts/local.d.ts"/>
 /// <reference path="d.ts/jvm/ts/navbar.d.ts"/>
 /// <reference path="d.ts/jvm/ts/reset.d.ts"/>
+/// <reference path="d.ts/threads/ts/threadsPlugin.d.ts"/>
+/// <reference path="d.ts/threads/ts/threads.d.ts"/>
